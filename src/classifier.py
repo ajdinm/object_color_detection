0f5de910eac7d75cb2e7a3a95309bb47cbfd1d79
Ajdin Mujezinović <amujezinov3@etf.unsa.ba>
2018-01-28 13:07:48 +0100

    refactor and add working example

--- src/classifier.py
+++ src/classifier.py
@@ -7,25 +7,27 @@ from random import shuffle
 
 import pickle
 
-def read_data(): 
+
+def read_data():
+    # Reads .csv data for RGB color dataset
     data_folder = '../data/colors/'
     files = []
     for (dirpath, dirnames, filenames) in walk(data_folder):
         files.extend(filenames)
         break
-
-#files = map(lambda x: data_folder + x, files)
     data = []
     for f in files:
         with open(data_folder + f) as csvfile:
-            reader = csv.reader(csvfile, delimiter=',', quotechar = '|')
+            reader = csv.reader(csvfile, delimiter=',', quotechar='|')
             name = f[:f.find('.')]
             for row in reader:
-                try: 
-                    data.append([name, map(lambda x: int(x), row[1].split(';'))])
+                try:
+                    data.append(
+                        [name, map(lambda x: int(x), row[1].split(';'))])
                 except:
                     continue
     return data
+
 
 def color_lookup(color_name):
     lookup = dict()
@@ -36,8 +38,10 @@ def color_lookup(color_name):
 
     return lookup[color_name]
 
+
 def reverse_lookup(predicted):
-    lookup = [['red', [1, 0, 0, 0]], ['green', [0, 1, 0, 0]], ['yellow', [0, 0, 1, 0]], ['blue', [0, 0, 0, 1]]]
+    lookup = [['red', [1, 0, 0, 0]], ['green', [0, 1, 0, 0]],
+              ['yellow', [0, 0, 1, 0]], ['blue', [0, 0, 0, 1]]]
     predicted = list(predicted)
     for x in lookup:
         if x[1] == predicted:
@@ -45,25 +49,29 @@ def reverse_lookup(predicted):
 
     return None
 
+
 def same_vector(x, y):
     for i in range(len(x)):
         if x[i] != y[i]:
             return False
     return True
 
+
 def normalize_rgb(rgb):
     rgb = map(lambda x: x / 255., rgb)
     return rgb
 
+
 def get_train_validation_data():
-    
+
     data = read_data()
     shuffle(data)
     color_names = ['red', 'blue', 'yellow', 'green']
 
     # red, blue, yellow, green
-    colors = map(lambda color: filter(lambda x: x[0] == color, data), color_names)
-    
+    colors = map(lambda color: filter(
+        lambda x: x[0] == color, data), color_names)
+
     # ensure same distribution of all classes
     min_len = min(map(lambda x: len(x), colors))
 
@@ -73,41 +81,43 @@ def get_train_validation_data():
         x = x + colors[i]
         y = y + [color_names[i]] * len(colors[i])
 
-    x = map(lambda xi: normalize_rgb(xi[1]), x) # put everything in [0, 1] range
+    x = map(lambda xi: normalize_rgb(
+        xi[1]), x)  # put everything in [0, 1] range
     y = map(lambda yi: color_lookup(yi), y)
-     
+
     validation_x = []
     validation_y = []
-    
+
     for i in range(len(color_names)):
         validation_x = validation_x + colors[i][min_len:]
-        validation_y = validation_y + [color_names[i]] * (len(colors[i]) - min_len)
+        validation_y = validation_y + \
+            [color_names[i]] * (len(colors[i]) - min_len)
 
     validation_x = map(lambda xi: normalize_rgb(xi[1]), validation_x)
     validation_y = map(lambda yi: color_lookup(yi), validation_y)
 
     return x, y, validation_x, validation_y
 
+
 def train_classifier():
 
-    clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes = (5, 3), random_state=1, activation='relu')
+    clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
+                        hidden_layer_sizes=(5, 3), random_state=1, activation='relu')
 
     train_x, train_y, valid_x, valid_y = get_train_validation_data()
 
     clf.fit(train_x, train_y)
     # save_classifier(clf)
-    # valid_x = 
     acc = map(lambda i, j: same_vector(i, j), clf.predict(valid_x), valid_y)
-    print acc.count(True) / float(len(acc))
+    # print acc.count(True) / float(len(acc))
 
 
-def save_classifier(clf):
-    filename = 'classifier.pkl'
+def save_classifier(clf, filename='classifier.pkl'):
     with open(filename, 'wb') as class_file:
         pickle.dump(clf, class_file)
 
-def load_classifier():
-    filename = 'classifier.pkl'
+
+def load_classifier(filename='classifier.pkl'):
     with open(filename, 'rb') as class_file:
         return pickle.load(class_file)
     return None
--- src/main.py
+++ src/main.py
@@ -10,10 +10,14 @@ import colorsys
 import classifier
 from os import walk
 
+
 def get_hue(x):
     return x * 360.0 / 179.0
 
+
 def get_rect(img_color):
+    # As BigBIRD dataset gives too large images, crop them to capture relevant
+    # part
     if img_color == 'yellow':
         return [495, 280, 320, 505]
     if img_color == 'green' or img_color == 'orange':
@@ -22,29 +26,21 @@ def get_rect(img_color):
         return [512, 245, 242, 504]
     return [570, 550, 145, 180]
 
-def get_imgs():
 
+def get_imgs():
+    # Requires dataset folder to be present in directory above.
+    # Inside that folder there should be N folders, named by color name. 
     data_folder = '../dataset/'
-    #imgs = [('coke.jpg', [570, 330, 120, 400], 3),
-    #        ('detergent.jpg', [545, 330, 220, 405], 3),
-    #        ('palmolive_green.jpg', [580, 430, 130, 310], 3),
-    #        ('red_cup.jpg', [570, 550, 145, 180], 3)]
 
     img_paths = []
     for (dirpath, dirnames, filenames) in walk(data_folder):
         color = dirpath.replace(data_folder, '')
         if dirpath != data_folder:
             # get full path to img and expected color
-            img_paths.extend(map(lambda x: [dirpath + '/'+x, get_rect(color), color], filenames))
+            img_paths.extend(map(lambda x: [dirpath + '/' + x, get_rect(color), color], filenames))
 
-    #imgs = map(lambda x: [data_folder+x, [545, 330, 220, 405], 'yellow'])
-    # make rects bigger
-    #for i in range(len(imgs)):
-    #    imgs[i][1][0] = imgs[i][1][0] - 50
-    #    imgs[i][1][1] = imgs[i][1][1] - 50
-    #    imgs[i][1][2] = imgs[i][1][2] + 100
-    #    imgs[i][1][3] = imgs[i][1][3] + 100
     return img_paths
+
 
 def predict_imgs():
 
@@ -55,46 +51,47 @@ def predict_imgs():
         filename = img[0]
         img_class = img[2]
         x, y, w, h = rect
-        
+
         img = cv2.imread(filename)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img = img[y:y+h, x:x+w] # get only relevant portion of the image
-        
-        results = results + [[img_class, colorsys.hsv_to_rgb(get_dominant_color(img) / 360., 1, 0.5)]]
+        img = img[y:y + h, x:x + w]  # get only relevant portion of the image
+
+        results = results + \
+            [[img_class, colorsys.hsv_to_rgb(
+                get_dominant_color(img) / 360., 1, 0.5)]]
     x = map(lambda xi: xi[1], results)
     y = map(lambda yi: yi[0], results)
     clf = classifier.load_classifier()
     predictions = map(lambda x: classifier.reverse_lookup(x), clf.predict(x))
-    predictions = map(lambda i: [predictions[i], y[i], results[i]], range(len(y)))
+    predictions = map(
+        lambda i: [predictions[i], y[i], results[i]], range(len(y)))
     failed_predictions = filter(lambda x: x[0] != x[1], predictions)
     print failed_predictions
     print 'accuracy', 1 - float(len(failed_predictions)) / len(predictions)
 
-def predict(filename):
 
-    
-    #x, y, w, h = [131, 3, 200, 400]
-    
+def predict(filename):
     img = cv2.imread(filename)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    #img = img[y:y+h, x:x+w] # get only relevant portion of the image
-    plt.imshow(img)
-    plt.show()
-        
+    # plt.imshow(img)
+    # plt.show()
+
     result = [colorsys.hsv_to_rgb(get_dominant_color(img) / 360., 1, 0.5)]
     clf = classifier.load_classifier()
     prediction = classifier.reverse_lookup(clf.predict(result)[0])
     print prediction
-        
-def get_dominant_color(img):
-        gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-        ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
-        zero_indx = np.where(thresh==0)
+
+def get_dominant_color(img):
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        ret, thresh = cv2.threshold(
+            gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+        zero_indx = np.where(thresh == 0)
         img[zero_indx] = [0, 0, 0]
-        
+
         img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-        Z = img.reshape((-1,3))
+        Z = img.reshape((-1, 3))
         hues = map(lambda x: x[0], Z)
         hue_counter = Counter(hues)
         if(len(hue_counter) > 1):
